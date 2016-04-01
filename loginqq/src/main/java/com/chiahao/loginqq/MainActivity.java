53728d28e6f1ea6d6833bde939c34683887024e6
路家豪 <xinruodingshui@gmail.com>
2016-04-01 14:03:46 +0800

    update

--- loginqq/src/main/java/com/chiahao/loginqq/MainActivity.java
+++ loginqq/src/main/java/com/chiahao/loginqq/MainActivity.java
@@ -1,9 +1,9 @@
 package com.chiahao.loginqq;
 
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,27 +13,29 @@ import android.widget.Toast;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText etNameGet,etPwdGet,etNamePost,etPwdPost;
-    private Button btnLoginGet,btnLoginPost;
-    private String userName,pwd;
+    private EditText etNameGet, etPwdGet, etNamePost, etPwdPost;
+    private Button btnLoginGet, btnLoginPost;
+    private String userName, pwd;
 
     private static final int SUCCESS = 1;
-    private Handler handler = new Handler(){
+    private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            switch (msg.what){
+            switch (msg.what) {
                 case SUCCESS:
-                    Toast.makeText(MainActivity.this,"服务器访问成功",Toast.LENGTH_SHORT).show();
-                break;
+                    Toast.makeText(MainActivity.this, (String) msg.obj, Toast.LENGTH_SHORT).show();
+                    break;
             }
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,33 +67,34 @@ public class MainActivity extends AppCompatActivity {
     private void doGet() {
         userName = etNameGet.getText().toString().trim();
         pwd = etPwdGet.getText().toString().trim();
-        String server = "http://192.168.91.2:80/LearnPHP/login/server.php";// 访问本机地址需要是静态地址
-        final String path = server + "?username="+userName+"&pwd="+pwd;
-        new Thread(){
+        final String server = "http://192.168.199.125:80/LearnPHP/login/GetServer.php";// 访问本机地址需要是静态地址
+        new Thread() {
             @Override
             public void run() {
                 try {
+                    String path = server + "?username=" + URLEncoder.encode(userName, "utf-8") + "&pwd=" + URLEncoder.encode(pwd, "utf-8");
                     URL url = new URL(path);
                     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                     conn.setRequestMethod("GET");
                     conn.setConnectTimeout(5000);
                     int responseCode = conn.getResponseCode();
-                    if (responseCode == 200){
+                    if (responseCode == 200) {
                         InputStream is = conn.getInputStream();
                         ByteArrayOutputStream baos = new ByteArrayOutputStream();
                         byte[] buffer = new byte[1024];
                         int len = -1;
-                        while ((len = is.read()) != -1){
-                            baos.write(buffer,0,len);
+                        while ((len = is.read(buffer)) != -1) {
+                            baos.write(buffer, 0, len);
                         }
                         is.close();
                         baos.close();
                         conn.disconnect();
-                        String result = new String(baos.toByteArray(),"UTF-8");
+                        String result = new String(baos.toByteArray(), "utf-8");
                         Message message = Message.obtain();
                         message.what = SUCCESS;
                         message.obj = result;
                         handler.sendMessage(message);
+                        Log.e("haha", result);
                     }
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
@@ -104,8 +107,59 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * POST方式访问网络
+     * TODO 我写的php后台还不能用  后面慢慢完善  明天先不用这个教她了
      */
     private void doPost() {
+        userName = etNamePost.getText().toString().trim();
+        pwd = etPwdPost.getText().toString().trim();
+        final String server = "http://192.168.199.125:80/LearnPHP/login/PostServer.php";// 访问本机地址需要是静态地址
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    //String path = server + "?username=" + URLEncoder.encode(userName, "utf-8") + "&pwd=" + URLEncoder.encode(pwd, "utf-8");
+                    URL url = new URL(server);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    // 1.设置请求方式为POST
+                    conn.setRequestMethod("POST");
+                    conn.setConnectTimeout(5000);
+                    // 2.设置http请求数据类型为表单类型
+                    conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                    // 3.设置给服务器的数据的长度-----即参数
+                    String params = "username=" + URLEncoder.encode(userName, "utf-8") + "&pwd=" + URLEncoder.encode(pwd, "utf-8");
+                    conn.setRequestProperty("Content-Length",String.valueOf(params));
+                    // 4.设置服务器可以写入数据
+                    conn.setDoOutput(true);
+                    // 5.开始向服务器写数据
+                    OutputStream os = conn.getOutputStream();
+                    os.write(params.getBytes());
 
+                    // 服务器响应
+                    int responseCode = conn.getResponseCode();
+                    if (responseCode == 200) {
+                        InputStream is = conn.getInputStream();
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        byte[] buffer = new byte[1024];
+                        int len = -1;
+                        while ((len = is.read(buffer)) != -1) {
+                            baos.write(buffer, 0, len);
+                        }
+                        is.close();
+                        baos.close();
+                        conn.disconnect();
+                        String result = new String(baos.toByteArray(), "utf-8");
+                        Message message = Message.obtain();
+                        message.what = SUCCESS;
+                        message.obj = result;
+                        handler.sendMessage(message);
+                        Log.e("haha", result);
+                    }
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
     }
 }
