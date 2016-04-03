417a1e8f52867feed27f88a3c578bee8a7681fdc
路家豪 <xinruodingshui@gmail.com>
2016-04-03 12:46:08 +0800

    媳妇update

--- loginqq/src/main/java/com/chiahao/loginqq/MainActivity.java
+++ loginqq/src/main/java/com/chiahao/loginqq/MainActivity.java
@@ -9,6 +9,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,6 +26,8 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
     private EditText etNameGet, etPwdGet, etNamePost, etPwdPost;
@@ -29,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         public void handleMessage(Message msg) {
             switch (msg.what) {
                 case SUCCESS:
-                    Toast.makeText(MainActivity.this, "服务器返回的数据-->"+(String) msg.obj, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "服务器返回的数据-->" + (String) msg.obj, Toast.LENGTH_SHORT).show();
                     break;
             }
         }
@@ -49,13 +60,17 @@ public class MainActivity extends AppCompatActivity {
         btnLoginGet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                doGet();
+//                doGet();
+                //doAsyncGet();
+                doXutilsGet();
             }
         });
         btnLoginPost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                doPost();
+                //doPost();
+                //doAsyncPost();
+                doXutilsPost();
             }
         });
     }
@@ -128,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
                     conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
                     // 3.设置给服务器的数据的长度-----即参数的长度
                     String params = "username=" + URLEncoder.encode(userName, "utf-8") + "&pwd=" + URLEncoder.encode(pwd, "utf-8");
-                    conn.setRequestProperty("Content-Length",String.valueOf(params.length()));// 这里要注意一定要传长度，不然会造成413响应码错误滴
+                    conn.setRequestProperty("Content-Length", String.valueOf(params.length()));// 这里要注意一定要传长度，不然会造成413响应码错误滴
                     // 4.设置服务器可以写入数据
                     conn.setDoOutput(true);
                     // 5.开始向服务器写数据
@@ -166,4 +181,92 @@ public class MainActivity extends AppCompatActivity {
             }
         }.start();
     }
+
+    private void doAsyncGet() {
+        userName = etNameGet.getText().toString().trim();
+        pwd = etPwdGet.getText().toString().trim();
+        String path = "http://192.168.173.2:80/LearnPHP/login/GetServer.php" + "?username=" + userName + "&pwd=" + pwd;
+        AsyncHttpClient client=new AsyncHttpClient();
+        client.get(path, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                Toast.makeText(MainActivity.this, "服务器响应成功" + new String(responseBody), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Toast.makeText(MainActivity.this, "服务器响应失败" + new String(responseBody), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+
+    private void doAsyncPost(){
+        userName = etNamePost.getText().toString().trim();
+        pwd = etPwdPost.getText().toString().trim();
+        final String server = "http://192.168.173.2:80/LearnPHP/login/PostServer.php";// 访问本机地址需要是静态地址
+        AsyncHttpClient client=new AsyncHttpClient();
+        RequestParams params=new RequestParams();
+        params.add("username",userName);
+        params.add("pwd",pwd);
+        client.post(server, params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                Toast.makeText(MainActivity.this, "服务器响应成功" + new String(responseBody), Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Toast.makeText(MainActivity.this, "服务器响应失败" + new String(responseBody), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    private void doXutilsGet(){
+        userName = etNameGet.getText().toString().trim();
+        pwd = etPwdGet.getText().toString().trim();
+        String path = "http://192.168.173.2:80/LearnPHP/login/GetServer.php" + "?username=" + userName + "&pwd=" + pwd;
+        HttpUtils utils=new HttpUtils();
+        utils.send(HttpRequest.HttpMethod.GET, path, new RequestCallBack<String>() {
+            @Override
+            public void onSuccess(ResponseInfo<String> responseInfo) {
+                Toast.makeText(MainActivity.this, "服务器响应成功" + responseInfo.result, Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onFailure(HttpException e, String s) {
+                Toast.makeText(MainActivity.this, "服务器响应失败" + s, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    private void doXutilsPost(){
+        userName = etNamePost.getText().toString().trim();
+        pwd = etPwdPost.getText().toString().trim();
+        final String server = "http://192.168.173.2:80/LearnPHP/login/PostServer.php";
+        com.lidroid.xutils.http.RequestParams params=new com.lidroid.xutils.http.RequestParams();
+        params.addBodyParameter("userName",userName);
+        params.addBodyParameter("pwd",pwd);
+
+        HttpUtils utils=new HttpUtils();
+        utils.send(HttpRequest.HttpMethod.POST, server, params, new RequestCallBack<Object>() {
+            @Override
+            public void onSuccess(ResponseInfo<Object> responseInfo) {
+                Toast.makeText(MainActivity.this, "服务器响应成功" + responseInfo.result, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(HttpException e, String s) {
+                Toast.makeText(MainActivity.this, "服务器响应失败" + s, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+
 }
